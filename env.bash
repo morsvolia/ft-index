2141fed4bf4aefbc85e6ce157a5174ca592960d5
User Macify <macify@seadragon.nix.bra>
2015-03-26 08:58:05 +0800

    add bash script to record switch options

---
+++ env.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ -d build ]; then
+  rm -rf build;
+fi
+
+mkdir build
+cd build
+
+export CC=clang
+export CXX=clang++
+export LINK=clang++
+
+export LDFLAGS="-L/usr/local/llvm33/lib -lkvm"
+#export LDFLAGS="-lexecinfo -lkvm"
+#export CXXFLAGS="-lexecinfo -lkvm"
+
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_EXE_LINKER_FLAGS="-L/usr/local/llvm33/lib -lkvm" \
+    -DUSE_VALGRIND=OFF \
+    -DTOKU_DEBUG_PARANOID=OFF \
+    -DBUILD_TESTING=OFF \
+    -DUSE_CTAGS=OFF \
+    -DUSE_ETAGS=OFF \
+    -DUSE_GTAGS=OFF \
+    -DUSE_CSCOPE=OFF \
+    -DCMAKE_INSTALL_PREFIXC=$PWD/install \
+    ..
+
+gmake install
